4d1569bd6e83c74d28ad7319876217d777c65646
Felix Kästner <kaestner.fe@gmail.com>
2019-11-05 20:28:43 +0100

    Add Button from react native source

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import { StyleSheet, TouchableOpacity, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  View,
+  Platform,
+  Text,
+} from 'react-native';
 
 export class Button extends Component {
   _handleOnPress = () => {
@@ -7,29 +14,117 @@ export class Button extends Component {
   };
 
   render() {
+    const {
+      accessibilityLabel,
+      color,
+      onPress,
+      touchSoundDisabled,
+      title,
+      hasTVPreferredFocus,
+      nextFocusDown,
+      nextFocusForward,
+      nextFocusLeft,
+      nextFocusRight,
+      nextFocusUp,
+      disabled,
+      outline,
+    } = this.props;
+    const buttonStyles = [styles.button];
+    const textStyles = [styles.text];
+    if (color) {
+      if (outline) {
+        textStyles.push({ color: color });
+      } else {
+        buttonStyles.push({ backgroundColor: color });
+      }
+    } else {
+      const defaultColor = '#57c75e';
+      if (outline) {
+        textStyles.push({ color: defaultColor });
+      } else {
+        buttonStyles.push({ backgroundColor: defaultColor });
+      }
+    }
+    const accessibilityState = {};
+    if (disabled) {
+      buttonStyles.push(styles.buttonDisabled);
+      textStyles.push(styles.textDisabled);
+      accessibilityState.disabled = true;
+    }
+
+    const formattedTitle =
+      Platform.OS === 'android' ? title.toUpperCase() : title;
+
+    const Touchable =
+      Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity;
+
     return (
-      <View style={styles.container}>
-        <TouchableOpacity
-          style={[styles.button, this.props.style]}
-          onPress={this._handleOnPress}>
-          {this.props.children}
-        </TouchableOpacity>
-      </View>
+      <Touchable
+        accessibilityLabel={accessibilityLabel}
+        accessibilityRole="button"
+        accessibilityState={accessibilityState}
+        hasTVPreferredFocus={hasTVPreferredFocus}
+        nextFocusDown={nextFocusDown}
+        nextFocusForward={nextFocusForward}
+        nextFocusLeft={nextFocusLeft}
+        nextFocusRight={nextFocusRight}
+        nextFocusUp={nextFocusUp}
+        disabled={disabled}
+        onPress={onPress}
+        touchSoundDisabled={touchSoundDisabled}
+        style={styles.container}>
+        <View style={buttonStyles}>
+          <Text style={textStyles} disabled={disabled}>
+            {formattedTitle}
+          </Text>
+        </View>
+      </Touchable>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingHorizontal: 10,
+    marginVertical: 10,
   },
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#57c75e',
-    padding: 10,
+  button: Platform.select({
+    ios: {},
+    android: {
+      elevation: 4,
+      // Material design blue from https://material.google.com/style/color.html#color-color-palette
+      backgroundColor: '#2196F3',
+      borderRadius: 2,
+    },
+  }),
+  text: {
+    textAlign: 'center',
+    margin: 8,
+    ...Platform.select({
+      ios: {
+        fontSize: 18,
+      },
+      android: {
+        fontWeight: '500',
+      },
+    }),
+    color: 'white',
+    fontSize: 18,
   },
+  buttonDisabled: Platform.select({
+    ios: {},
+    android: {
+      elevation: 0,
+      backgroundColor: '#dfdfdf',
+    },
+  }),
+  textDisabled: Platform.select({
+    ios: {
+      color: '#cdcdcd',
+    },
+    android: {
+      color: '#a1a1a1',
+    },
+  }),
 });
 
 export default Button;
