8e3514b10956e63c78f9c1a277922bc0ee2f238a
Felix Kästner <kaestner.fe@gmail.com>
2019-11-06 19:36:23 +0100

    add qr code scanner and gps location finder

---
+++ src/components/QRCodeScanner/QRCodeScanner.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
+
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+export class QRCodeScanner extends Component {
+  state = {
+    hasCameraPermission: null,
+    scanned: false,
+    errorMessage: null,
+  };
+
+  async componentDidMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage:
+          'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this._getPermissionsAsync();
+    }
+  }
+
+  _getPermissionsAsync = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+        hasCameraPermission: status === 'granted',
+      });
+    } else {
+      this.setState({ hasCameraPermission: status === 'granted' });
+    }
+  };
+
+  render() {
+    const { hasCameraPermission, scanned } = this.state;
+
+    if (hasCameraPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
+    return (
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'flex-end',
+        }}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : this._handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+
+        {scanned && (
+          <Button
+            title={'Tap to Scan Again'}
+            onPress={() => this.setState({ scanned: false })}
+          />
+        )}
+      </View>
+    );
+  }
+
+  _handleBarCodeScanned = ({ type, data }) => {
+    this.setState({ scanned: true });
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+}
+
+export default QRCodeScanner;
