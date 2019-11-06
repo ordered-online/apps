81d8a53b3b797343d942393927b62855f24cc75e
Felix Kästner <kaestner.fe@gmail.com>
2019-11-06 20:42:50 +0100

    Fix missing import

--- src/components/QRCodeScanner/QRCodeScanner.js
+++ src/components/QRCodeScanner/QRCodeScanner.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button, Platform } from 'react-native';
 import Constants from 'expo-constants';
 import * as Permissions from 'expo-permissions';
 
