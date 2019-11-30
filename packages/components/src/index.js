dc46bee66cad45c3f1bede35fbcf08eb3d2cbb2f
Felix Kästner <kaestner.fe@gmail.com>
2019-11-30 16:55:40 +0100

    Add a generic Card Component

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -1,6 +1,7 @@
 export { default as Anchor } from './Anchor';
 export { default as BrowserLink } from './BrowserLink';
 export { default as Button } from './Button';
+export { default as Card } from './Card';
 export { default as Input } from './Input';
 export { default as Link } from './Link';
 export { default as LocationFinder } from './LocationFinder';
