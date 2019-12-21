35750e2f161d1866f5a4293dd62d7eb1cd8ead10
Felix Kästner <kaestner.fe@gmail.com>
2019-12-21 16:37:52 +0100

    Fix webpack hot reloading and add current dev task for vscode

--- webpack.config.js
+++ webpack.config.js
@@ -56,18 +56,18 @@ module.exports = async function(env, argv) {
   });
 
   if (config.mode === 'development') {
-    config.devServer.proxy = {
-      '/**': {
-        target: {
-          host: 'localhost',
-          protocol: 'http:',
-          port: 80,
-        },
-        secure: false,
-        changeOrigin: true,
-        logLevel: 'info',
-      },
-    };
+    // config.devServer.proxy = {
+    //   '/**': {
+    //     target: {
+    //       host: 'localhost',
+    //       protocol: 'http:',
+    //       port: 80,
+    //     },
+    //     secure: false,
+    //     changeOrigin: true,
+    //     logLevel: 'info',
+    //   },
+    // };
     config.devServer.historyApiFallback = true;
   }
 
