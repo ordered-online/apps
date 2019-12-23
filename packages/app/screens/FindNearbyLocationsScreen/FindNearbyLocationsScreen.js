e311487ce1bbc4060749f91da2920db7c095560e
Felix Kästner <kaestner.fe@gmail.com>
2019-12-23 05:16:58 +0100

    sketch initial barcode scanner and location finder in customer app

--- packages/app/screens/FindNearbyLocationsScreen/FindNearbyLocationsScreen.js
+++ packages/app/screens/FindNearbyLocationsScreen/FindNearbyLocationsScreen.js
@@ -2,10 +2,48 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import Constants from 'expo-constants';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
 import { FindLocationNearby } from '../../store/locations';
 
 export class FindNearbyLocationsScreen extends Component {
+  constructor(props) {
+    super(props);
+    state = {
+      location: null,
+      errorMessage: null,
+    };
+
+    this.getLocationPermissions = this.getLocationPermissions.bind(this);
+  }
+
+  componentDidMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage:
+          'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this.getLocationPermissions();
+    }
+  }
+
+  async getLocationPermissions() {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+  }
+
+  async getLocation() {
+    let location = await Location.getCurrentPositionAsync();
+    this.setState({ location });
+  }
+
   render() {
     return (
       <View>
--- packages/app/screens/OrderScreen/OrderScreen.js
+++ packages/app/screens/OrderScreen/OrderScreen.js
@@ -1,9 +1,85 @@
 import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
 
 export class OrderScreen extends Component {
+  constructor(props) {
+    super(props);
+    state = {
+      hasCameraPermission: null,
+      scanned: false,
+      errorMessage: null,
+    };
+
+    this.getCameraPermissions = this.getCameraPermissions.bind(this);
+    this.handleBarCodeScanned = this.handleBarCodeScanned.bind(this);
+  }
+
+  async componentDidMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage:
+          'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this._getPermissionsAsync();
+    }
+  }
+
+  async getCameraPermissions() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+        hasCameraPermission: status === 'granted',
+      });
+    } else {
+      this.setState({ hasCameraPermission: status === 'granted' });
+    }
+  }
+
+  handleBarCodeScanned({ type, data }) {
+    this.setState({ scanned: true });
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    this.props.onBarCodeScanned && this.props.onBarCodeScanned(data);
+  }
+
   render() {
-    return <View style={styles.container}></View>;
+    const { hasCameraPermission, scanned } = this.state;
+
+    if (hasCameraPermission === null) {
+      return (
+        <View>
+          <Text>Requesting for camera permission</Text>
+        </View>
+      );
+    }
+    if (hasCameraPermission === false) {
+      return (
+        <View>
+          <Text>No access to camera</Text>
+        </View>
+      );
+    }
+
+    return (
+      <View style={styles.container}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : this._handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+
+        {scanned && (
+          <Button
+            color="#57c75e"
+            title={'Tap to Scan Again'}
+            onPress={() => this.setState({ scanned: false })}
+          />
+        )}
+      </View>
+    );
   }
 }
 
