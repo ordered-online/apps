90282342b4b9984d797faa410fd062431791a44d
Felix Kästner <kaestner.fe@gmail.com>
2019-12-29 01:29:33 +0100

    refractor monorepo & workspace packages and expo version

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,5 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    babelrcRoots: [
-      // Keep the root as a root
-      '.',
-      // Also consider monorepo packages "root" and load their .babelrc files.
-      './packages/*',
-    ],
   };
 };
