bf579d1dfd0743070d3e8bff345c20c96bc175be
PhilippMatthes <github@philippmatth.es>
2020-01-25 13:37:51 +0100

    Remove unpublishable text from imprint

--- packages/app/screens/AboutScreen/AboutScreen.js
+++ packages/app/screens/AboutScreen/AboutScreen.js
@@ -5,7 +5,7 @@ import { Button, Text } from '@ordered.online/components';
 
 export class AboutScreen extends Component {
   static navigationOptions = {
-    title: 'Terms, Privacy and Imprint',
+    title: 'Imprint',
     headerStyle: {
       backgroundColor: '#f8f8f8',
       borderBottomLeftRadius: 24,
@@ -22,30 +22,9 @@ export class AboutScreen extends Component {
       <ScrollView
         style={styles.container}
         contentContainerStyle={styles.contentContainerStyle}>
-        <Text h2 style={styles.headline}>
-          Imprint
-        </Text>
         <Text style={styles.content}>
           The ordered.online© platform was developed by {'\n'}Philipp Matthes
-          and Felix Kästner
-        </Text>
-        <Text h2 style={styles.headline}>
-          Terms of Use
-        </Text>
-        <Text style={styles.content}>All rights reserved</Text>
-        <Text h2 style={styles.headline}>
-          Privacy Policy
-        </Text>
-        <Text style={styles.content}>
-          We proudly capture all of your data and sell it to satan.
-        </Text>
-        <Text>
-          <Button
-            raised
-            title="Contact us"
-            titleStyle={{ color: '#fff', width: '100%' }}
-            onPress={() => Linking.openURL('mailto:contact@ordered.online')}
-          />
+          and Felix Kästner Contact: contact@ordered.online
         </Text>
       </ScrollView>
     );
