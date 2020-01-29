5193ca1986a9e277b179e8c01a1d0470e90249af
Felix Kästner <kaestner.fe@gmail.com>
2020-01-29 22:14:56 +0100

    fix tests

--- packages/manager/__tests__/manager.test.js
+++ packages/manager/__tests__/manager.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import App from '../App';
+import App from '../App.js';
 
 jest.mock('expo', () => ({
   AppLoading: 'AppLoading',
